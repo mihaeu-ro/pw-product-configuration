7603842dfeb09ff91de42dc4d257d28e958fc78c
Michael Haeuslmann <haeuslmann@gmail.com>
2015-11-30 09:56:00 +0100

    Remove unused methods

--- tests/UIdTest.php
+++ tests/UIdTest.php
@@ -11,14 +11,4 @@ class UIdTest extends PHPUnit_Framework_TestCase
         $uidB = new UId(['test']);
         $this->assertNotEquals($uidA->uid(), $uidB->uid());
     }
-    
-    public function setUp()
-    {
-        parent::setUp();
-    }
-
-    public function tearDown()
-    {
-        parent::tearDown();
-    }
 }
